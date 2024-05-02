839b97b970fdce2b28f17cb4b7b46d7ade3d9692
Abdulqodir <abdulqodirmaxmadaliyev03@gmail.com>
2024-05-02 12:49:28 +0500

    site

---
+++ #dev/js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('#dev')
+})
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
